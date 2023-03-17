4096004eb280049c2e4d817c319b363b0b8063b5
Алсу Фаизова <silicium.xxx@gmail.com>
2023-03-18 00:10:46 +0300

    homework 3

--- SimpleClassifier/config/cfg.py
+++ SimpleClassifier/config/cfg.py
@@ -4,4 +4,3 @@ cfg = EasyDict()
 cfg.nb_football_player = 500
 cfg.nb_basketball_player = 500
 cfg.max_height = 230
-
--- SimpleClassifier/model/simple_classifier.py
+++ SimpleClassifier/model/simple_classifier.py
@@ -1,6 +1,7 @@
 from config.cfg import cfg
 
-class Classifier():
+
+class Classifier:
     def __call__(self, height):
         """returns confidence of belonging to the class of basketball players"""
         return height/cfg.max_height
