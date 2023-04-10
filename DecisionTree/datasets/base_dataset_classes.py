0843c56e8baf41a4ddb8c10503e8b4a4ba978dca
Алсу Фаизова <silicium.xxx@gmail.com>
2023-04-11 00:56:05 +0300

    homework 5

---
+++ DecisionTree/datasets/base_dataset_classes.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class BaseDataset(ABC):
+
+    def __init__(self, train_set_percent, valid_set_percent):
+        self.targets_test = None
+        self.targets_valid = None
+        self.targets_train = None
+        self.inputs_test = None
+        self.inputs_valid = None
+        self.inputs_train = None
+        self.train_set_percent = train_set_percent
+        self.valid_set_percent = valid_set_percent
+
+    @property
+    @abstractmethod
+    def targets(self):
+        # targets variables
+        pass
+
+    @property
+    @abstractmethod
+    def inputs(self):
+        # inputs variables
+        pass
+
+    @property
+    @abstractmethod
+    def d(self):
+        # inputs variables
+        pass
+
+    def divide_into_sets(self):
+        # перемешивание
+        randomize = np.arange(self.inputs.shape[0])
+        np.random.shuffle(randomize)
+        inputs = self.inputs[randomize]
+        targets = self.targets[randomize]
+        # разделение
+        index_1 = int(inputs.shape[0] * self.train_set_percent)
+        index_2 = int(inputs.shape[0] * (self.train_set_percent + self.valid_set_percent))
+        self.inputs_train, self.inputs_valid, self.inputs_test = np.split(inputs, [index_1, index_2])
+        self.targets_train, self.targets_valid, self.targets_test = np.split(targets, [index_1, index_2])
+
+    def normalization(self):
+        # write normalization method BONUS TASK
+        min_input = np.min(self.inputs, axis=0)  # axis - calculate by each dimension
+        max_input = np.max(self.inputs, axis=0)  # axis 0 - ищем максимум в столбце
+        difference = max_input - min_input
+        difference[difference == 0] = 1
+        self.inputs_train = (self.inputs_train - min_input) / difference
+        self.inputs_valid = (self.inputs_valid - min_input) / difference
+        self.inputs_test = (self.inputs_test - min_input) / difference
+
+    def __get_data_stats(self):
+        # calculate mean and std of inputs vectors of training set by each dimension
+        mean = np.mean(self.inputs_train, axis=0)
+        std = np.std(self.inputs_train, axis=0)
+        std[std == 0] = 1
+        return mean, std
+
+    def standardization(self):
+        # write standardization method (use stats from __get_data_stats)
+        mean, std = self.__get_data_stats()
+        self.inputs_train = (self.inputs_train - mean) / std
+        self.inputs_valid = (self.inputs_valid - mean) / std
+        self.inputs_test = (self.inputs_test - mean) / std
+        return mean, std
+
+
+class BaseClassificationDataset(BaseDataset):
+
+    @property
+    @abstractmethod
+    def k(self):
+        # number of classes
+        pass
+
+    @staticmethod
+    def onehotencoding(targets: np.ndarray, number_classes: int) -> np.ndarray:
+        # create matrix of onehot encoding vectors for input targets
+        targets = np.array(targets).reshape(-1)
+        return np.eye(number_classes)[targets]
--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -1,1 +1,57 @@
+import utils.metrics as metrics
+from config.decision_tree_config import cfg as des_tree_cfg
+from datasets.digits_dataset import Digits
+from datasets.wine_quality_dataset import WineQuality
+from models.decision_tree import DT
+from utils.enums import SetType, TaskType
 
+
+def experiment_classification():
+    digits = Digits(des_tree_cfg)
+    decision_tree = DT(task_type=TaskType.classification,
+                       max_depth=7,
+                       min_entropy=0.01,
+                       min_number_of_elem=1)
+    decision_tree.train(digits(SetType.train)['inputs'], digits(SetType.train)['targets'])
+
+    predictions_valid = decision_tree.get_predictions(digits(SetType.valid)['inputs'])
+    confusion_matrix_valid = metrics.confusion_matrix(predictions_valid, digits(SetType.valid)['targets'])
+    accuracy_valid = metrics.accuracy(confusion_matrix_valid)
+    print('Confusion matrix on valid set:')
+    print(confusion_matrix_valid)
+    print(f'Accuracy on valid set: {accuracy_valid}')
+
+    print()
+
+    predictions_test = decision_tree.get_predictions(digits(SetType.test)['inputs'])
+    confusion_matrix_test = metrics.confusion_matrix(predictions_test, digits(SetType.test)['targets'])
+    accuracy_test = metrics.accuracy(confusion_matrix_test)
+    print('Confusion matrix on test set:')
+    print(confusion_matrix_test)
+    print(f'Accuracy on test set: {accuracy_test}')
+
+
+def experiment_regression():
+    wine_quality = WineQuality(des_tree_cfg)
+    decision_tree = DT(task_type=TaskType.regression,
+                       max_depth=10,
+                       min_entropy=0.01,
+                       min_number_of_elem=1)
+    decision_tree.train(wine_quality(SetType.train)['inputs'], wine_quality(SetType.train)['targets'])
+
+    predictions_valid = decision_tree.get_predictions(wine_quality(SetType.valid)['inputs'])
+    mean_squared_error_valid = metrics.MSE(predictions_valid, wine_quality(SetType.valid)['targets'])
+    print(f'Error value on valid set: {mean_squared_error_valid}')
+
+    predictions_test = decision_tree.get_predictions(wine_quality(SetType.test)['inputs'])
+    # targets_test = wine_quality(SetType.test)['targets']
+    # for i in range(len(targets_test)):
+    #     print(f'{targets_test[i]} {predictions_test[i]}')
+    mean_squared_error_test = metrics.MSE(predictions_test, wine_quality(SetType.test)['targets'])
+    print(f'Error value on test set: {mean_squared_error_test}')
+
+
+if __name__ == '__main__':
+    experiment_classification()
+    print()
+    experiment_regression()
